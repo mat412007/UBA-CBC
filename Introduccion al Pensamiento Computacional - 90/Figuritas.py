570c5d893c67f92f9f4a29852faadde38fe925c3
Appaz <matias2007play4@gmail.com>
2026-08-29 15:20:23 -0300

    Ejercicio Figuritas terminado

--- Introduccion al Pensamiento Computacional - 90/Figuritas.py
+++ Introduccion al Pensamiento Computacional - 90/Figuritas.py
@@ -7,34 +7,43 @@ def cantidad_para_llenar(tam):
         figu = random.randint(0,tam-1)
         album[figu] = 1
         cant_figus = cant_figus + 1
-    return cant_figus
+    return cant_figus # Cantidad de figuritas que compramos para llenar un album
 
 # --------------------------------------------------
 
 def cuantas_figus_multiple(figus_total, n_albumes):
-    repeticiones = [0]*n_albumes
+    albumes = [0]*n_albumes
     contador = 0
     
     while contador < n_albumes:
         
-        repeticiones[contador] = cantidad_para_llenar(figus_total)
+        albumes[contador] = cantidad_para_llenar(figus_total)
         contador += 1
         
-    return repeticiones
+    return albumes # Lista de cantidades de figuritas que compramos para llenar el mismo album
 
 # --------------------------------------------------
 
 def promedio_figus(figus_total, n_albumes):
     cantidades = cuantas_figus_multiple(figus_total, n_albumes)
-    print(cantidades)
     
-    return sum(cantidades) // n_albumes
+    return sum(cantidades) / n_albumes # Promedio de cuantas figurtas nos toma llenar un album, usando una lista de intentos
 
 # --------------------------------------------------
 
-repeticiones = 5
-tam_album = 6
+def probar_todos_tams(figus_min, figus_max, repeticiones):
+    listaPromedios = [0]*(figus_max-figus_min)
+    tam_album = figus_min
+    
+    while tam_album < figus_max:
+        listaPromedios[tam_album-figus_min] = promedio_figus(tam_album, repeticiones)
+        tam_album += 1
+    
+    return listaPromedios # Lista de promedios de albumes de diferentes tamaños, sin contar el ultimo
 
-promedio = promedio_figus(tam_album, repeticiones)
-print(promedio)
+# --------------------------------------------------
+
+
+lista = probar_todos_tams(4, 7, 100)
+print(lista)
 

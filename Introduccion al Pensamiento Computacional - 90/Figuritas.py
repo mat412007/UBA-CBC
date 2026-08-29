4c13cf79e6009a7755387fcd3a4c6f2643ca82b1
Appaz <matias2007play4@gmail.com>
2026-08-29 14:49:36 -0300

    Demo ejercicio figuritas

--- Introduccion al Pensamiento Computacional - 90/Figuritas.py
+++ Introduccion al Pensamiento Computacional - 90/Figuritas.py
@@ -1,36 +1,40 @@
 import random
 
-def llenarAlbum(nFiguritas):
-    album = [0]*nFiguritas # Lista de n ceros
-    cantFiguritas = 0 # Cantidad de figuritas compradas para llenar el album
+def cantidad_para_llenar(tam):
+    album = [0]*tam
+    cant_figus = 0
+    while sum(album)<len(album):
+        figu = random.randint(0,tam-1)
+        album[figu] = 1
+        cant_figus = cant_figus + 1
+    return cant_figus
+
+# --------------------------------------------------
+
+def cuantas_figus_multiple(figus_total, n_albumes):
+    repeticiones = [0]*n_albumes
+    contador = 0
+    
+    while contador < n_albumes:
         
-    while sum(album) < nFiguritas:
-        figurita = random.randint(0,nFiguritas-1) # aleatorio entre 0 y size-1
-        album[figurita] = 1 # llenamos el espacio correspondiente a la figurta
-        cantFiguritas+=1 # vamos contando la cantidad de intentos
+        repeticiones[contador] = cantidad_para_llenar(figus_total)
+        contador += 1
+        
+    return repeticiones
 
-    return cantFiguritas
+# --------------------------------------------------
 
-
-nFiguritas = int(input("\nCual es el tamaño del album? : "))
-albumes = int(input("Cuantos albumes deseas llenar? : "))
-
-intentos = [0]*albumes
-intento = 0
-
-while intento < albumes:
+def promedio_figus(figus_total, n_albumes):
+    cantidades = cuantas_figus_multiple(figus_total, n_albumes)
+    print(cantidades)
     
-    intentos[intento] = llenarAlbum(nFiguritas)
-    intento +=1
-    
-print("\nEstos fueron los intentos:")
-print(intentos) # Cantidades de figuritas compradas para llenar los albumes
+    return sum(cantidades) // n_albumes
 
-promedio = sum(intentos)//len(intentos)
-print("\nEl promedio de intentos es " + str(promedio))
+# --------------------------------------------------
 
+repeticiones = 5
+tam_album = 6
 
-# lista.append()
-# sum(suma)
-# len(largo)
-# and, or, not
+promedio = promedio_figus(tam_album, repeticiones)
+print(promedio)
+

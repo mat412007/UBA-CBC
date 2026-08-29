88b7e9aa3ccbd2946f3849f082261d315ba78fd4
Appaz <matias2007play4@gmail.com>
2026-08-29 13:58:12 -0300

    Funcion de llenado

--- Introduccion al Pensamiento Computacional - 90/Figuritas.py
+++ Introduccion al Pensamiento Computacional - 90/Figuritas.py
@@ -1,24 +1,35 @@
 import random
 
-intentos = [0]*10
-intento = 0
-
-while intento < len(intentos):
-    album = [0]*6 # Lista de 6 ceros
-    cantFiguritas = 0
-    
-    while sum(album) < 6:
-        figurita = random.randint(0,5) # aleatorio entre 0 y 5
+def llenarAlbum(nFiguritas):
+    album = [0]*nFiguritas # Lista de n ceros
+    cantFiguritas = 0 # Cantidad de figuritas compradas para llenar el album
+        
+    while sum(album) < nFiguritas:
+        figurita = random.randint(0,nFiguritas-1) # aleatorio entre 0 y size-1
         album[figurita] = 1 # llenamos el espacio correspondiente a la figurta
         cantFiguritas+=1 # vamos contando la cantidad de intentos
-    intentos[intento] = cantFiguritas
+
+    return cantFiguritas
+
+
+nFiguritas = int(input("\nCual es el tamaño del album? : "))
+albumes = int(input("Cuantos albumes deseas llenar? : "))
+
+intentos = [0]*albumes
+intento = 0
+
+while intento < albumes:
+    
+    intentos[intento] = llenarAlbum(nFiguritas)
     intento +=1
     
 print("\nEstos fueron los intentos:")
-print(intentos)
+print(intentos) # Cantidades de figuritas compradas para llenar los albumes
 
 promedio = sum(intentos)//len(intentos)
 print("\nEl promedio de intentos es " + str(promedio))
+
+
 # lista.append()
 # sum(suma)
 # len(largo)

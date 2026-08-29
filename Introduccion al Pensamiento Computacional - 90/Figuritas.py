2db37f286bf223a90d8ee8fa54b5c9623e0906ed
Appaz <matias2007play4@gmail.com>
2026-08-29 13:18:20 -0300

    Promedio de intentos

--- Introduccion al Pensamiento Computacional - 90/Figuritas.py
+++ Introduccion al Pensamiento Computacional - 90/Figuritas.py
@@ -1,16 +1,24 @@
 import random
 
-album = [0]*6 # Lista de 6 ceros
-intentos = 0
+intentos = [0]*10
+intento = 0
 
-while sum(album) < 6:
-    figurita = random.randint(0,5) # aleatorio entre 0 y 5
-    album[figurita] = 1 # llenamos el espacio correspondiente a la figurta
-    intentos+=1 # vamos contando la cantidad de intentos
+while intento < len(intentos):
+    album = [0]*6 # Lista de 6 ceros
+    cantFiguritas = 0
     
-print(album)
-print("Cantidad de intentos: " + str(intentos))
+    while sum(album) < 6:
+        figurita = random.randint(0,5) # aleatorio entre 0 y 5
+        album[figurita] = 1 # llenamos el espacio correspondiente a la figurta
+        cantFiguritas+=1 # vamos contando la cantidad de intentos
+    intentos[intento] = cantFiguritas
+    intento +=1
+    
+print("\nEstos fueron los intentos:")
+print(intentos)
 
+promedio = sum(intentos)//len(intentos)
+print("\nEl promedio de intentos es " + str(promedio))
 # lista.append()
 # sum(suma)
 # len(largo)
